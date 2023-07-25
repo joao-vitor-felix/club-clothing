945b9c530bf6d754b146e8d1d43f284f6f0948d7
João Vitor <joaovitorfelixcontanto@gmail.com>
2023-07-25 17:42:15 -0300

    fix: fixed application height

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -11,17 +11,22 @@ export const GlobalStyles = createGlobalStyle`
 
 html {
   font-size: 62.5%;
+  height: 100%;
 }
 
 body {
   box-sizing: border-box;
   font-family: 'Poppins', sans-serif;
   height: 100%;
-  color: ${({ theme }) => theme.text.primary};
+  color: ${({ theme }) => theme.text.secondary};
 }
 
 a {
   color: ${({ theme }) => theme.text.primary};
   text-decoration: none;
+}
+
+#root {
+  height: 100%;
 }
 `;
