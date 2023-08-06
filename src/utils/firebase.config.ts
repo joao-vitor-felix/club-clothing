2b9b3ad93d1f856a1b81207468486481728caf91
João Vitor <joaovitorfelixcontanto@gmail.com>
2023-08-06 19:00:07 -0300

    chore: added firebase config

---
+++ src/utils/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBumn98PKZwAvpeRHossFb-eotIeqyh9ps",
+  authDomain: "club-e-commerce.firebaseapp.com",
+  projectId: "club-e-commerce",
+  storageBucket: "club-e-commerce.appspot.com",
+  messagingSenderId: "603360256717",
+  appId: "1:603360256717:web:54c5c387af0e221040ff84"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
