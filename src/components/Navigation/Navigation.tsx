9125c591b42e5d5bcdbb9b0494d9e7781e131008
João Vitor <joaovitorfelixcontanto@gmail.com>
2023-08-09 21:12:33 -0300

    feat: added dynamic nav links relying on user state

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,12 +3,15 @@ import * as S from "./Navigation.styles";
 import { BsCart3 } from "react-icons/bs";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/firebase.config";
+import useUserContext from "../../hooks/useUserContext";
 
 const Navigation = () => {
   const SignUserOut = async () => {
     await signOut(auth);
     console.log("User logged out");
   };
+
+  const { currentUser } = useUserContext();
 
   return (
     <>
@@ -19,13 +22,15 @@ const Navigation = () => {
         </S.LogoContainer>
         <S.ItemWrapper>
           <S.Item to="/">Explorar</S.Item>
-          <S.Item to="sign-up">Criar conta</S.Item>
-          {auth.currentUser ? (
+          {currentUser ? (
             <S.Item to="#" onClick={SignUserOut}>
               Sair
             </S.Item>
           ) : (
-            <S.Item to="sign-in">Entrar</S.Item>
+            <>
+              <S.Item to="sign-up">Criar conta</S.Item>
+              <S.Item to="sign-in">Entrar</S.Item>
+            </>
           )}
 
           <S.Item to="/">
