a39bc5eaf6d4108543b0f846dfb67f506b420545
João Vitor <joaovitorfelixcontanto@gmail.com>
2023-08-12 19:21:05 -0300

    feat: added Category context

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./contexts/UserContext.tsx";
+import CategoryContextProvider from "./contexts/CategoryContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserContextProvider>
+    <CategoryContextProvider>
+      <UserContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserContextProvider>
+    </CategoryContextProvider>
   </React.StrictMode>
 );
