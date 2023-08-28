12c63c667b0753d9ae4e4ec92398cafc5d7a0668
João Vitor <joaovitorfelixcontanto@gmail.com>
2023-08-28 17:05:32 -0300

    feat: added stripe integration

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -3,9 +3,32 @@ import Button from "../../components/Button/Button";
 import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
 import useCartContext from "../../hooks/useCartContext";
 import * as S from "./Checkout.styles";
+import { useState } from "react";
+import axios from "axios";
 
 const Checkout = () => {
   const { cart } = useCartContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const finishPurchase = async () => {
+    try {
+      setIsLoading(true);
+
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/create-checkout-session`,
+        {
+          cart
+        }
+      );
+
+      window.location.href = data.url;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <S.Container>
       <S.Text>Checkout</S.Text>
@@ -14,7 +37,13 @@ const Checkout = () => {
           {cart.map(item => (
             <CheckoutItem product={item} key={item.id} />
           ))}
-          <Button icon={<BsCartPlus size={23} />}>Finalizar compra</Button>
+          <Button
+            icon={<BsCartPlus size={23} />}
+            onClick={finishPurchase}
+            isLoading={isLoading}
+          >
+            Finalizar compra
+          </Button>
         </>
       ) : (
         <S.Empty>Seu carrinho está vazio :( </S.Empty>
