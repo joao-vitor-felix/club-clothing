93baeae2e8512a60ef6022bafb454a1b37b01e4d
João Vitor <joaovitorfelixcontanto@gmail.com>
2023-08-29 11:38:27 -0300

    chore: added redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,21 @@ import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./contexts/UserContext.tsx";
 import CategoryContextProvider from "./contexts/CategoryContext.tsx";
 import CartContextProvider from "./contexts/CartContext.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <CategoryContextProvider>
-        <CartContextProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </CartContextProvider>
-      </CategoryContextProvider>
-    </UserContextProvider>
+    <Provider store={store}>
+      <UserContextProvider>
+        <CategoryContextProvider>
+          <CartContextProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </CartContextProvider>
+        </CategoryContextProvider>
+      </UserContextProvider>
+    </Provider>
   </React.StrictMode>
 );
