d59c65409c9741899877c8c43e8f60d182db154e
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-07 21:50:38 -0300

    test: added tests to CartItem

---
+++ src/components/Cart/components/CartItem/CartItem.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from "@testing-library/react";
+import Cart from "../../../../types/cart.type";
+import { renderWithRedux } from "../../../../utils/test.helper";
+import CartItem from "./CartItem";
+
+describe("Cart Item", () => {
+  it("should show correct cart item", () => {
+    const cartItem: Cart = {
+      id: "1",
+      imageUrl: "image_url",
+      name: "Boné",
+      price: 100,
+      quantity: 1
+    };
+
+    renderWithRedux(<CartItem product={cartItem} />, {});
+
+    const item = screen.getByText("Boné");
+    const price = screen.getByText("R$ 100");
+    const quantity = screen.getByText("1");
+    const increaseButton = screen.getByLabelText("Aumentar Boné");
+    const decreaseButton = screen.getByLabelText("Diminuir Boné");
+    const removeButton = screen.getByLabelText("Remover Boné");
+    expect(item).toBeInTheDocument();
+    expect(price).toBeInTheDocument();
+    expect(quantity).toBeInTheDocument();
+    expect(increaseButton).toBeInTheDocument();
+    expect(decreaseButton).toBeInTheDocument();
+    expect(removeButton).toBeInTheDocument();
+  });
+});
