4cd7557bce3b14f1338380ce4e6e1b05fcc02d13
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-08 11:30:16 -0300

    test: added test to ProductItem

---
+++ src/components/ProductItem/ProductItem.test.tsx
@@ -0,0 +1,58 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { renderWithRedux } from "../../utils/test.helper";
+import ProductItem from "./ProductItem";
+import Navigation from "../Navigation/Navigation";
+import Product from "../../types/product.types";
+
+describe("ProductItem", () => {
+  it("should add product to cart when click on product item button", async () => {
+    const product: Product = {
+      id: "1",
+      name: "Boné",
+      imageUrl: "google.com.br",
+      price: 10
+    };
+
+    renderWithRedux(
+      <>
+        <Navigation />
+        <ProductItem product={product} />
+      </>,
+      { preloadedState: { cart: { cartItems: [] } } as any }
+    );
+
+    const cart = screen.getByLabelText("Carrinho de compras");
+    const quantity = screen.queryByText("0");
+    const message = screen.queryByText("Seu carrinho está vazio!");
+
+    expect(cart).toBeInTheDocument();
+    expect(quantity).toBeInTheDocument();
+    expect(message).toBeInTheDocument();
+
+    const cartButton = screen.getByLabelText("Adicionar Boné ao carrinho");
+
+    await userEvent.click(cartButton);
+    await userEvent.click(cartButton);
+
+    expect(cart).toHaveTextContent("2");
+  });
+
+  it("should show correct product", () => {
+    const product: Product = {
+      id: "1",
+      name: "Boné",
+      imageUrl: "google.com.br",
+      price: 10
+    };
+
+    renderWithRedux(<ProductItem product={product} />, {});
+
+    const item = screen.getByText("Boné");
+    const price = screen.getByText("R$10");
+    const button = screen.getByText("Adicionar ao carrinho");
+    expect(item).toBeInTheDocument();
+    expect(price).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+});
--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -2,8 +2,7 @@ import { FC } from "react";
 import * as S from "./ProductItem.styles";
 import Product from "../../types/product.types";
 import { BsCartPlus } from "react-icons/bs";
-import { useDispatch, useSelector } from "react-redux";
-import { selectCartItems } from "../../store/cart/cart.selectors";
+import { useDispatch } from "react-redux";
 import { addProductToCart } from "../../store/cart/cart.slice";
 
 type ProductItemProps = {
@@ -12,13 +11,15 @@ type ProductItemProps = {
 
 const ProductItem: FC<ProductItemProps> = ({ product }) => {
   const { imageUrl, name, price } = product;
-  const cart = useSelector(selectCartItems);
   const dispatch = useDispatch();
 
   return (
     <S.Container>
       <S.Image $imageUrl={imageUrl}>
-        <S.CartButton onClick={() => dispatch(addProductToCart(product))}>
+        <S.CartButton
+          onClick={() => dispatch(addProductToCart(product))}
+          aria-label={`Adicionar ${name} ao carrinho`}
+        >
           <BsCartPlus size={23} /> Adicionar ao carrinho
         </S.CartButton>
       </S.Image>
