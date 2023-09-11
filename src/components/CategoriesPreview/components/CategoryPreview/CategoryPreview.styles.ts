e023ddcfedb4271995727d27d7860bf7ed41597c
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-11 18:01:45 -0300

    style: added responsiveness to CategoryPreview

--- src/components/CategoriesPreview/components/CategoryPreview/CategoryPreview.styles.ts
+++ src/components/CategoriesPreview/components/CategoryPreview/CategoryPreview.styles.ts
@@ -6,12 +6,13 @@ export const Container = styled.div`
   flex-direction: column;
   width: 100%;
   margin-top: 2rem;
+  gap: 1rem;
 `;
 
 export const Title = styled(Link)`
   width: max-content;
   color: ${({ theme }) => theme.text.secondary};
-  font-size: 2.2rem;
+  font-size: 2.6rem;
   font-weight: 500;
   margin-bottom: 0.5rem;
 `;
@@ -19,6 +20,12 @@ export const Title = styled(Link)`
 export const ProductsContainer = styled.div`
   display: flex;
   width: 100%;
-  justify-content: space-between;
+  justify-content: center;
+  gap: 2.5rem;
   flex-wrap: wrap;
+
+  @media screen and (max-width: 768px) {
+    /* gap: 3rem; */
+    /* justify-content: center; */
+  }
 `;
