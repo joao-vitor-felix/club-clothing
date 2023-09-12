845b7da77ca77be183a23ab607604cd81fc6ff2c
João Vitor <joaovitorfelixcontato@gmail.com>
2023-09-12 18:53:53 -0300

    perf: added lazy loading to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { GlobalStyles } from "./GlobalStyles";
 import { ToastContainer } from "react-toastify";
@@ -9,14 +9,20 @@ import { userConverter } from "./firebase/firestore.converters";
 import { useDispatch } from "react-redux";
 import Theme from "./Theme";
 import Navigation from "./components/Navigation/Navigation";
-import Home from "./pages/Home/Home";
-import SignIn from "./pages/SignIn/SignIn";
-import SignUp from "./pages/SignUp/SignUp";
-import Shop from "./pages/Shop/Shop";
-import Checkout from "./pages/Checkout/Checkout";
-import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
-import PaymentConfirmation from "./pages/PaymentConfirmation/PaymentConfirmation";
 import { loginUser } from "./store/user/user.slice";
+import Loading from "./components/Loading/Loading";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
+const SignUp = lazy(() => import("./pages/SignUp/SignUp"));
+const Shop = lazy(() => import("./pages/Shop/Shop"));
+const Checkout = lazy(() => import("./pages/Checkout/Checkout"));
+const ProtectedRoute = lazy(
+  () => import("./components/ProtectedRoute/ProtectedRoute")
+);
+const PaymentConfirmation = lazy(
+  () => import("./pages/PaymentConfirmation/PaymentConfirmation")
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -42,26 +48,28 @@ function App() {
     <Theme>
       <GlobalStyles />
       <ToastContainer style={{ fontSize: "1.8rem" }} />
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path="shop/*" element={<Shop />} />
-          <Route
-            path="checkout"
-            element={
-              <ProtectedRoute>
-                <Checkout />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="sign-in" element={<SignIn />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route
-            path="payment-confirmation"
-            element={<PaymentConfirmation />}
-          />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path="shop/*" element={<Shop />} />
+            <Route
+              path="checkout"
+              element={
+                <ProtectedRoute>
+                  <Checkout />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="sign-in" element={<SignIn />} />
+            <Route path="sign-up" element={<SignUp />} />
+            <Route
+              path="payment-confirmation"
+              element={<PaymentConfirmation />}
+            />
+          </Route>
+        </Routes>
+      </Suspense>
     </Theme>
   );
 }
